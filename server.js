64224abc8e19344487dce05d621ca2db8f117683
Magno <magnocruz@proton.me>
2023-03-04 16:47:15 -0300

    Logging a User Out 01

--- server.js
+++ server.js
@@ -152,12 +152,10 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    //Change the response to render the Pug template
     res.render('index', {
       title: 'Connected to Database',
-      message: 'Please login',
+      message: 'Please log in',
       showLogin: true
     });
   });
@@ -168,6 +166,17 @@ myDB(async client => {
 
   app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render('profile', { username: req.user.username });
+  });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   passport.use(new LocalStrategy((username, password, done) => {
