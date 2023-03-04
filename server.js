cc1152497d91ffb220e655c3d8a8e64b0708ec02
Magno <magnocruz@proton.me>
2023-03-04 15:39:09 -0300

    How to Use Passport Strategies 04

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict';
+/*'use strict';
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-//const routes = require('./routes.js');
-//const auth = require('./auth.js');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const passportSocketIo = require('passport.socketio')
 const cookieParser = require('cookie-parser')
@@ -54,80 +54,7 @@ myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   
  
-  //routes.js
-  app.route('/').get((req, res) => {
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please log in',
-      showLogin: true,
-      //showRegistration: true,
-	  //showSocialAuth: true
-    });
-  });
-  //app.route('/auth/github')
-  //.post(passport.authenticate('github', (req,res) => {
-  //}));
-  //app.route('/auth/github/callback')
-  //.post(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
-    //req.session.user_id = req.user.id
-	//res.redirect('/chat');
-  //});
-
-  //app.route('/chat')
-  //.post(passport.ensureAuthenticated('chat.pug', { user: req.user }), (req,res) => {
-    //res.redirect('/profile');
-  //});
-
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/profile');
-  });
-
-  app.route('/profile').get((req,res) => {
-    res.render('profile');
-  });
-
-  app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
-
-  app.route('/register').post((req, res, next) => {
-    const hash = bcrypt.hashSync(req.body.password, 12);
-    myDataBase.findOne({ username: req.body.username }, (err, user) => {
-      if (err) {
-        next(err);
-      } else if (user) {
-        res.redirect('/');
-      } else {
-        myDataBase.insertOne({
-          username: req.body.username,
-          password: hash
-        },
-          (err, doc) => {
-            if (err) {
-              res.redirect('/');
-            } else {
-              // The inserted document is held within
-              // the ops property of the doc
-              next(null, doc.ops[0]);
-            }
-          }
-        )
-      }
-    })
-  },
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res, next) => {
-      res.redirect('/profile');
-    }
-  );
-
-  app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
-  });
-  //routes.js
+  
   
   routes(app, myDataBase);
   auth(app, myDataBase);
@@ -147,78 +74,6 @@ myDB(async client => {
 
   console.log('A user has connected');
     
-//auth.js
-
-//require('dotenv').config();
-
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
-const { ObjectID } = require('mongodb');
-const GitHubStrategy = require('passport-github').Strategy
-
-module.exports = function (app, myDataBase) {
-  passport.serializeUser((user, done) => {
-      done(null, user._id);
-  });
-  passport.deserializeUser((id, done) => {
-      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-          //if (err) return console.error(err);
-          //done(null, doc);
-		  done(null, null);
-      });
-  });
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!bcrypt.compareSync(password, user.password)) { 
-          return done(null, false);
-      }
-      return done(null, user);
-    });
-  }));
-
-
-  passport.use(new GitHubStrategy({
-	clientID: process.env.GITHUB_CLIENT_ID,
-	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	callbackURL: 'https://boilerplate-advancednode.magnocruz.repl.co/auth/github/callback'
-  },
-	function(accessToken, refreshToken, profile, cb) {
-	  console.log(profile);
-	  myDataBase.findOneAndUpdate(
-		{ id: profile.id },
-		{
-		  $setOnInsert: {
-			id: profile.id,
-			username: profile.username,
-			name: profile.displayName || 'John Doe',
-			photo: profile.photos[0].value || '',
-			email: Array.isArray(profile.emails)
-			  ? profile.emails[0].value
-			  : 'No public email',
-			created_on: new Date(),
-			provider: profile.provider || ''
-		  },
-		  $set: {
-			last_login: new Date()
-		  },
-		  $inc: {
-			login_count: 1
-		  }
-		},
-		{ upsert: true, new: true },
-		(err, doc) => {
-		  return cb(null, doc.value);
-		}
-	  );	}
-  ));
-}
-
-//auth.js
-
 
 
   	console.log('A user has disconnected');
@@ -262,5 +117,84 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
+*/
 
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in',
+      showLogin: true
+    });
+  });
+
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  })
+
+  app.route('/profile').get((req,res) => {
+    res.render('profile');
+  })
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
