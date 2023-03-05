eb10fb5271014a63a23c03ecad420a87a7841a81
Magno <magnocruz@proton.me>
2023-03-05 12:43:07 -0300

    Handle a Disconnect 01

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const passport = require('passport');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
 
-const passportSocketIo = require('passport.socketio')
-const cookieParser = require('cookie-parser')
-const MongoStore = require('connect-mongo') (session);
-const URI = process.env.MONGO_URI;
-const store = new MongoStore({ url: URI });
-
 const app = express();
 
 const http = require('http').createServer(app);
@@ -26,9 +20,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false },
-  key: 'express.sid',
-  store: store
+  cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
@@ -39,50 +31,32 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-io.use(
-	passportSocketIo.authorize({
-	  cookieParser: cookieParser,
-	  key: 'express.sid',
-	  secret: process.env.SESSION_SECRET,
-	  store: store,
-	  success: onAuthorizeSuccess,
-	  fail: onAuthorizeFail
-	})
-  );
-
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
+
   routes(app, myDataBase);
   auth(app, myDataBase);
 
   let currentUsers = 0;
   io.on('connection', (socket) => {
-	++currentUsers;
-	io.emit('user count', currentUsers);
-	console.log('A user has connected');
-  });
+    ++currentUsers;
+    io.emit('user count', currentUsers);
+    console.log('A user has connected');
 
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    });
+  });
+  
 }).catch(e => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: 'Hello', message: 'Please log in' });
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-
-function onAuthorizeSuccess(data, accept) {
-	console.log('successful connection to socket.io');
   
-	accept(null, true);
-  }
-  
-  function onAuthorizeFail(data, message, error, accept) {
-	if (error) throw new Error(message);
-	console.log('failed connection to socket.io:', message);
-	accept(null, false);
-  }
-
-//console.log('user ' + socket.request.user.username + ' connected');
-
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
